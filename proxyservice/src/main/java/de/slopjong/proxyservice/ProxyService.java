ad7d99ef9d39af19655a5820b1dfc25f42f115af
slopjong <git@slopjong.de>
2012-09-21 17:03:15 +0200

    Fixed a timeout issue.

--- proxyservice/src/main/java/de/slopjong/proxyservice/ProxyService.java
+++ proxyservice/src/main/java/de/slopjong/proxyservice/ProxyService.java
@@ -1,7 +1,5 @@
 package de.slopjong.proxyservice;
 
-import javax.xml.namespace.QName;
-
 import org.apache.axiom.om.OMAbstractFactory;
 import org.apache.axiom.om.OMElement;
 import org.apache.axiom.om.OMFactory;
@@ -11,7 +9,11 @@ import org.apache.axis2.addressing.EndpointReference;
 import org.apache.axis2.client.Options;
 import org.apache.axis2.client.ServiceClient;
 import org.apache.axis2.client.async.AxisCallback;
+import org.apache.axis2.context.ConfigurationContext;
 import org.apache.axis2.context.MessageContext;
+import org.apache.axis2.transport.http.HTTPConstants;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
 
 /**
  * The proxy service.
@@ -24,7 +26,6 @@ public class ProxyService
     throws AxisFault
     {   
     	String argument = "<math xmlns=\"http://www.w3.org/1998/Math/MathML\"><apply><times/><cn>13</cn><cn>2</cn></apply></math>";
-    	//String endpoint = "http://93.92.148.20:8080/services/MathService";
     	String action = "urn:calculate";
     	
     	String endpoints[] = {
@@ -35,9 +36,7 @@ public class ProxyService
     	
         ServiceClient client = null;
     	OMFactory factory = OMAbstractFactory.getOMFactory();
-    	
-
-    	
+     	
     	/*
     	AxisService service = new AxisService();
     	AxisEndpoint endpoint = new AxisEndpoint();
@@ -54,14 +53,24 @@ public class ProxyService
         	OMElement value = factory.createOMElement("math", omNs); 
         	value.setText(argument); 
         	method.addChild(value);
-        	
-    		System.out.println(endpoint);
     		
 	        try
 	        {
 	        	client = new ServiceClient();
-		        client.cleanupTransport();
-		        client.cleanup();
+	        	
+	        	//client.getOptions().setProperty(HTTPConstants.REUSE_HTTP_CLIENT, "true");
+	        	
+	        	
+	        	// See [0]
+	        	ConfigurationContext configurationContext = client.getServiceContext()
+	        														.getConfigurationContext();
+
+	        	// increase the MAX possible parallel connections
+	        	MultiThreadedHttpConnectionManager conmgr = new MultiThreadedHttpConnectionManager();
+	        	conmgr.getParams().setDefaultMaxConnectionsPerHost(20);
+	        	HttpClient httpclient = new HttpClient(conmgr);
+	        	configurationContext.setProperty(HTTPConstants.CACHED_HTTP_CLIENT, httpclient);
+				
 	        }
 	        catch ( AxisFault e )
 	        { 
@@ -72,6 +81,7 @@ public class ProxyService
 	        Options opts = new Options();
 	        opts.setTo( new EndpointReference(endpoint) );
 	        opts.setAction(action);
+	        
 	        client.setOptions( opts );
 	        
 	        try
@@ -94,19 +104,23 @@ public class ProxyService
 		        	}
 	
 		        	public void onMessage(MessageContext arg0) 
-		        	{
-		        		EndpointReference ref = arg0.getFrom();
-		        		System.out.println(ref.getAddress());
+		        	{		 
+		        		// TODO: some of the following lines triggers onError
+		        		//EndpointReference ref = arg0.getFrom();
+		        		//System.out.println(ref.getAddress());
 		        		//String soapAction = arg0.getSoapAction(); // urn:calculateResponse
 		        		//String wsaAction = arg0.getWSAAction(); // calculateResponse
 		        		//Options options = arg0.getOptions();
+		        		
 		        		OMElement serviceResponse = arg0.getEnvelope().getBody().getFirstElement();
 		        		OMElement methodReturn = serviceResponse.getFirstElement();
 		        		
 		        		System.out.println(methodReturn.getText());
 		        	}
 				});
-		        
+		       
+		        client.cleanupTransport();
+		        //client.cleanup();
 	        }
 	        catch ( AxisFault e )
 	        {
@@ -115,3 +129,7 @@ public class ProxyService
     	}
     }
 }
+
+/*
+ [0] http://axis.apache.org/axis2/java/core/docs/http-transport.html#setting_cached_httpclient_object
+*/
